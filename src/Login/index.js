7ca4b3cc65a8eb0b89a848751a6092e1a0f095fe
Gustavo Diogo <gustavodiogo.aluno@unipampa.edu.br>
2021-11-25 12:47:51 -0300

    remove: node_modules .expo
    Removidos do repositório, yarn no console para baixar tudo de volta.

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,23 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useEffect} from 'react';
+import axios from 'axios'
 import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
 
 export default function LogIn() {
+
+    useEffect(() => {
+        axios.get('http://192.168.0.110:3030/api/users').then(function (response) {
+            const lista = response.data;
+
+            console.log(response.data)
+        })
+    });
+
     return (
         <View>
             <View>
                 <Text>LogIn</Text>
-                <TextInput placeholder="LogIn"/>
+                <TextInput placeholder="LogIn" />
             </View>
             <View>
                 <Text>Password</Text>
-                <TextInput placeholder="Password"/>
+                <TextInput placeholder="Password" />
             </View>
             <View>
                 <Button
                     title="LogIn"
                     color="#f194ff"
-                    onPress={() => Alert.alert("rsrs")}
+                    onPress={() => {}}
                 />
             </View>
         </View>
