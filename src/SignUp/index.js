f18dedce17542bf216c40ca4edfe8f4c75064b28
Gustavo Diogo <gustavodiogo.aluno@unipampa.edu.br>
2021-11-26 13:52:44 -0300

    feat: Fmenu for main Home after auth.

--- src/SignUp/index.js
+++ src/SignUp/index.js
@@ -1,31 +1,126 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import axios from "axios";
 
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+export default function SignUp({ navigation }) {
+  const [customerName, setCustomerName] = useState("");
+  const [customerAddress, setCustomerAddress] = useState("");
+  const [customerEmail, setCustomerEmail] = useState("");
+  const [customerPassword, setCustomerPassword] = useState("");
+  const [customerBirthdayDay, setCustomerBirthdayDay] = useState("");
+  const [customerBirthdayMonth, setCustomerBirthdayMonth] = useState("");
+  const [customerBirthdayYear, setCustomerBirthdayYear] = useState("");
 
+  function registerUser(
+    customerName,
+    customerAddress,
+    customerEmail,
+    customerPassword,
+    customerBirthdayDay,
+    customerBirthdayMonth,
+    customerBirthdayYear
+  ) {
+    const date =
+      customerBirthdayDay +
+      "-" +
+      customerBirthdayMonth +
+      "-" +
+      customerBirthdayYear;
+    axios
+      .post("http://192.168.0.110:3030/api/users", {
+        customerName: customerName,
+        customerAddress: customerAddress,
+        customerEmail: customerEmail,
+        customerPassword: customerPassword,
+        customerBirthday: date,
+      })
+      .then(function (response) {
+        if (response.status === 201) {
+          navigation.navigate("Fmenu");
+        }
+        if (response.status === 400) {
+          console.log("Usuário já registrado ");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-export default function SignUp() {
-    return (
+  
+
+  return (
+    <View>
+      <View>
+        <Text>Username</Text>
+        <TextInput
+          placeholder="Place your name please"
+          onChangeText={setCustomerName}
+          value={customerName}
+        />
+      </View>
+      <View>
+        <Text>Password</Text>
+        <TextInput
+          placeholder="Choose a password"
+          onChangeText={setCustomerPassword}
+          value={customerPassword}
+          secureTextEntry
+        />
+      </View>
+      <View>
+        <Text>E-Mail</Text>
+        <TextInput
+          placeholder="Choose an email address"
+          keyboardType='email-address'
+          onChangeText={setCustomerEmail}
+          value={customerEmail}
+        />
+        <Text>Endereço</Text>
+        <TextInput
+          placeholder="Choose an email address"
+          onChangeText={setCustomerAddress}
+          value={customerAddress}
+        />
         <View>
-            <View>
-                <Text>Username</Text>
-                <TextInput placeholder="Choose a Username"/>
-            </View>
-            <View>
-                <Text>Password</Text>
-                <TextInput placeholder="Choose a Password"></TextInput>
-            </View>
-            <View>
-                <Text>E-Mail</Text>
-                <TextInput placeholder="Type ur e-mail"/>
-            </View>      
-            <View>
-                <Button
-                    title="SignUp"
-                    color="#f194ff"
-                    onPress={() => Alert.alert("rsrs")}
-                />
-            </View>
-            
+          <Text>Data de Nascimento</Text>
+          <TextInput
+            placeholder="Day of Birth"
+            keyboardType="numeric"
+            onChangeText={setCustomerBirthdayDay}
+            value={customerBirthdayDay}
+          />
+          <TextInput
+            placeholder="Month of Birth"
+            keyboardType="numeric"
+            onChangeText={setCustomerBirthdayMonth}
+            value={customerBirthdayMonth}
+          />
+          <TextInput
+            placeholder="Year of Birth"
+            keyboardType="numeric"
+            onChangeText={setCustomerBirthdayYear}
+            value={customerBirthdayYear}
+          />
         </View>
-    );
+      </View>
+      <View>
+        <Button
+          title="SignUp"
+          color="#f194ff"
+          onPress={() =>
+            registerUser(
+              customerName,
+              customerAddress,
+              customerEmail,
+              customerPassword,
+              customerBirthdayDay,
+              customerBirthdayMonth,
+              customerBirthdayYear
+            )
+          }
+        />
+      </View>
+    </View>
+  );
 }
