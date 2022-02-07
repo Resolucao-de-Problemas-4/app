ac22f50265f7046c9665cde75b8e029c1314a092
Gustavo Diogo <67334321+Gustavo-Diogo@users.noreply.github.com>
2022-02-07 09:05:26 -0300

    feature: delete card.

--- src/card/index.js
+++ src/card/index.js
@@ -8,7 +8,8 @@ import {
     Modal,
     Dimensions,
     TouchableOpacity,
-    FlatList
+    FlatList,
+    Alert,
 } from "react-native";
 
 import { Ionicons } from '@expo/vector-icons';
@@ -21,10 +22,11 @@ import { tokenInfoCliente } from "../token";
 
 
 export default function CardScreen() {
-    const [cardList, setCardList] = useState('');
+    const [cardList, setCardList] = useState([]);
+
     useEffect(() => {
         axios.post(API_REST + '' + PORT + '/api/card-list', {
-            token: tokenInfoCliente.token
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQyZTIxNDcyLTMwZTgtNDQyMi04ZjIzLTZkZTY2NmNiNWIxOSIsImlhdCI6MTY0NDIzMTQzMiwiZXhwIjoxNjQ0MzE3ODMyfQ.HdCgWQk5293ODCbkGn8umnJYAphv2Lp6brNHn6JP4n8'
         }).then(function (response) {
             setCardList(response.data)
 
@@ -33,39 +35,92 @@ export default function CardScreen() {
         })
 
     }, []);
+
+    function excludeCard(cardID) {
+        function finExclude(){
+            axios.post(API_REST+''+PORT+''+'/api/card-delete',{
+                ID:cardID,
+                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQyZTIxNDcyLTMwZTgtNDQyMi04ZjIzLTZkZTY2NmNiNWIxOSIsImlhdCI6MTY0NDIzMTQzMiwiZXhwIjoxNjQ0MzE3ODMyfQ.HdCgWQk5293ODCbkGn8umnJYAphv2Lp6brNHn6JP4n8'
+            }).then(function (response){
+                if(response.status === 200){
+                    Alert.alert('Cartão Deletado!')
+                    axios.post(API_REST + '' + PORT + '/api/card-list', {
+                        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQyZTIxNDcyLTMwZTgtNDQyMi04ZjIzLTZkZTY2NmNiNWIxOSIsImlhdCI6MTY0NDIzMTQzMiwiZXhwIjoxNjQ0MzE3ODMyfQ.HdCgWQk5293ODCbkGn8umnJYAphv2Lp6brNHn6JP4n8'
+                    }).then(function (response) {
+                        setCardList(response.data)
+            
+                    }).catch(function (error) {
+                        setCardList(null)
+                    })
+                }
+            }).catch(function(error){
+                Alert.alert('Algo deu errado, tente novamente!')
+            })
+        }
+        Alert.alert(
+            "Alert Title",
+            "My Alert Msg",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => Alert.alert("Cancelado!"),
+                    style: "cancel",
+                },
+                {
+                    text:'Deletar!',
+                    onPress: ()=> finExclude()
+                }
+            ],
+            {
+                cancelable: true,
+                onDismiss: () =>
+                    Alert.alert(
+                        "Exclusão Cancelada"
+                    ),
+            }
+        );
+    }
+
     return (
 
-        <View>
-            <View >
-                <Text style={{ fontSize: 32, textAlign: 'center' }}>SEUS CARTÕES</Text>
+        <View style={[styles.container, { flexDirection: 'column' }]}>
 
-                <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', position: 'relative', marginTop: '100%',}}>
-                        <FlatList
-                        horizontal
-                            data={cardList}
-                            keyExtractor={cardList => cardList.ID}
-                            renderItem={({ item }) => {
-                                return (
-                                    <View style={{ flexDirection: 'row', width: '40%', borderWidth: 3, borderRadius: 20, marginTop: 10, height: 100 }}>
-                                        <Ionicons name="card-sharp" size={36} color="black" style={{ alignSelf:'center'}} />
-                                        <View style={{width:'100%', alignSelf:'center', marginLeft:10}}>
-                                            <Text>
-                                                {item.nome}
-                                            </Text>
-                                            <Text>
-                                                {item.numero}
-                                            </Text> 
-                                        </View>
-                                    </View>
-                                )
-                            }}
+            <Text style={{ fontSize: 32, textAlign: 'center' }}>SEUS CARTÕES</Text>
 
 
-                        />
+            <FlatList
+                horizontal
+                data={cardList}
+                keyExtractor={cardList => cardList.ID}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.viewFlat}>
+                            <View style={{ alignSelf: 'flex-end', marginTop: 0, marginRight: 4, position: 'absolute', }}>
 
-                </View>
-            </View>
-        </View>
+                                <TouchableOpacity onPress={() => excludeCard(item.ID)}>
+                                    <Ionicons name="trash-outline" size={24} color="red" style={{ alignSelf: 'center' }} />
+                                </TouchableOpacity>
+                            </View>
+                            <Ionicons name="card-sharp" size={36} color="black" style={{ alignSelf: 'center' }} />
+                            <View>
+                                <Text style={styles.textFlat}>
+                                    {item.nome}
+                                </Text>
+                                <Text style={styles.textFlat}>
+                                    {item.dataValidade}
+                                </Text>
+                                <Text style={styles.textFlat}>
+                                    {item.numero}
+                                </Text>
+                            </View>
+                        </View>
+                    )
+                }}
+            />
+
+
+
+        </View >
 
     )
 }
@@ -81,5 +136,25 @@ const styles = StyleSheet.create({
         height: '100%',
 
     },
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    viewFlat: {
+        borderWidth: 2,
+        borderColor: '#ccc',
+        padding: 40,
+        marginRight: 4,
+        marginLeft: 4,
+        height: '30%',
+        top: 450,
+
+    },
+    textFlat: {
+        fontSize: 20,
+        padding: 10,
+        textAlign: 'center'
+    }
+
 
 })
